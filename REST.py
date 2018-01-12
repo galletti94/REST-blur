50f55a029148781e4cf37ecc7d99c80103bb3dad
galletti94 <galletti@bu.edu>
2018-01-12 09:32:34 -0500

    Updating: adding api objects and methods

--- REST.py
+++ REST.py
@@ -7,12 +7,34 @@ db_connect = create_engine(os.environ['DATABASE_URL'])
 app = Flask(__name__)
 api = Api(app)
 
+class Models_id(Resource):
+    def get(self):
+        conn = db_connect.connect()
+        query = conn.execute('SELECT * FROM models')
+        return {'models': [i[0] for i in query.cursor.fetchall()]}
 
 
+class Models_weight(Resource):
+    def get(self, model_type):
+        conn = db_connect.connect()
+        query = conn.execute('SELECT weights FROM models WHERE model_type= %s' (model_type))
+        return {'weights': [i[0] for i in query.cursor.fetchall()]}
 
+class Images(Resource):
+    def get(self):
+        conn = db_connect.connect()
+        query = conn.execute('SELECT * FROM images')
+        return {'images': [i[0] for i in query.cursor.fetchall()]}
 
-
-
+    def post(self):
+        conn = db_connect.connect()
+        img = request.json['img']
+        label = request.json['label']
+        try:
+            query = conn.execute('INSERT INTO images(img, label) VALUES (%s, %s)' (img, label))
+            return {'status':'success'}
+        except Exception e:
+            return {'status':'error %s' e}
 
 
 
